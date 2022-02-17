56c227c57241e4f06b7ea793665c61531a67b314
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-17 09:41:14 -0500

    [STYLE] list of user with packages

---
+++ components/Packages/PackagesList.jsx
@@ -0,0 +1,89 @@
+import { Input, Avatar, Col, Spacer, Row, Text } from "@nextui-org/react";
+
+const defaultAvatar =
+  "https://i.pinimg.com/236x/29/68/3b/29683b54520b500a531ad18a4534c85e.jpg";
+
+const packageListCss = {
+  display: "flex",
+  gap: "1rem",
+  width: "100%",
+  justifyContent: "flex-start",
+  "@sm": {
+    flexDirection: "column",
+    alignItems: "center",
+    gap: ".5rem",
+    overflow: "auto",
+  },
+};
+
+const packageItemCss = {
+  bg: "$accents2",
+  p: ".75rem",
+  borderRadius: "1rem",
+  cursor: "pointer",
+  width: "auto",
+  display: "flex",
+  flexDirection: "column",
+  gap: ".5rem",
+  "@sm": {
+    w: "95%",
+    flexDirection: "row",
+    p: ".5rem",
+  },
+};
+
+export default function PackagesList() {
+  return (
+    <>
+      <Col>
+        <form>
+          <Input
+            placeholder="Find your package"
+            type="search"
+            bordered
+            color="secondary"
+            helperText="min 3 characters"
+            name="keyword"
+            id="SearchInput"
+            arial-label="Search"
+            css={{ bg: "#333", w: "95%", mx: "auto" }}
+          />
+        </form>
+        <Spacer y={1.5} />
+        <Col css={packageListCss}>
+          {[...new Array(5)].map((_, i) => (
+            <Row
+              key={i}
+              align="center"
+              justify="space-around"
+              css={packageItemCss}
+            >
+              <Avatar size="md" src={defaultAvatar} />
+              <Text
+                h6
+                size="14px"
+                css={{
+                  display: "none",
+                  "@sm": {
+                    display: "block",
+                  },
+                }}
+              >
+                User name
+              </Text>
+              <Text size="12px">15hs</Text>
+            </Row>
+          ))}
+        </Col>
+      </Col>
+      <style jsx>{`
+        form {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </>
+  );
+}
--- pages/packages/index.jsx
+++ pages/packages/index.jsx
@@ -1,3 +1,20 @@
+import { Grid } from "@nextui-org/react";
+
+import PackagesList from "../../components/Packages/PackagesList";
+
 export default function Packages() {
-  return <div>Packages</div>;
+  return (
+    <>
+      <h1>Packages </h1>
+      <Grid.Container>
+        <Grid xs={12} sm={3}>
+          <PackagesList />
+        </Grid>
+        <Grid xs={12} sm={9}>
+          Map
+        </Grid>
+      </Grid.Container>
+      <style jsx>{``}</style>
+    </>
+  );
 }
