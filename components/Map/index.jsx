42797bf0436ceab524d0d4abc1bdc024f4efe86d
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-17 13:35:26 -0500

    add custom `height` to map

--- components/Map/index.jsx
+++ components/Map/index.jsx
@@ -1,9 +1,9 @@
 import { useRef, useState, useEffect } from "react";
+import { Col } from "@nextui-org/react";
 import mapboxgl from "!mapbox-gl";
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiaXRzbWVsZW9uYXJkbyIsImEiOiJja3ZzcmpoMTU0czBtMm9tbHZhbTMzYTFrIn0.fgwmkMuUlIaqcnXtQjbWfQ";
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
-export default function Map() {
+export default function Map({ height }) {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-70.9);
@@ -18,26 +18,30 @@ export default function Map() {
       center: [lng, lat],
       zoom: zoom,
     });
-  });
+  }, []);
 
   useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
+    if (!map.current) return;
     map.current.on("move", () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
-  });
+  }, [map]);
 
   return (
     <>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
+      <Col css={{ position: "relative" }}>
+        <div className="sidebar">
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+        <div ref={mapContainer} className="map-container" />
+      </Col>
       <style jsx>{`
         .map-container {
-          height: 400px;
+          position: relative;
+          height: ${height};
+          width: 100%;
         }
 
         .sidebar {
